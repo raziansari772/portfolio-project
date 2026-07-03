5cce5c6369be0604f2dcb13094087afd65d16490
Razi Ansari <raziansari596@gmail.com>
2026-07-03 10:52:58 +0500

    ADD To Portfolio

---
+++ js.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("#btn");
+const msg = document.querySelector("#msg");
+
+btn.addEventListener("click", function(){
+
+        msg.textContent = "Thanks! I will contact you soon 🚀";
+    
+});
